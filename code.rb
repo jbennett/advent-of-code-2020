8e143a43cecd03201f92fd1924812afce71c70b5
Jonathan Bennett <jonathan@jbennett.me>
2020-12-08 03:41:40 +0000

    day 3b

--- code.rb
+++ code.rb
@@ -2,20 +2,24 @@ grid = File.read('day-03-input').split("\n").map { |x| x.split "" }
 
 rows = grid.count
 cols = grid[0].count
-x_step = 3
-y_step = 1
-x = y = 0
-trees = 0
 
-loop do
-	y += y_step
-	x += x_step
-	break if y >= rows
+slopes = [[1,1], [3, 1], [5,1], [7,1], [1, 2]].map do |x_step, y_step|
+	x = y = 0
+	trees = 0
 	
-	wrapped_x = x % cols
-	wrapped_y = y % rows
+	loop do
+		y += y_step
+		x += x_step
+		break if y >= rows
+		
+		wrapped_x = x % cols
+		wrapped_y = y % rows
+		
+		trees += 1 if grid[wrapped_y][wrapped_x] == '#'
+	end
 	
-	trees += 1 if grid[wrapped_y][wrapped_x] == '#'
+	trees
 end
 
-puts trees
+puts slopes.inspect
+puts slopes.inject(:*)
