801dda9bd7ab969d4afb5aa28e09d1c10a4358bd
Jonathan Bennett <jonathan@jbennett.me>
2020-12-08 03:24:45 +0000

    day 2b

--- code.rb
+++ code.rb
@@ -2,15 +2,13 @@ passwords = File.read('day-2-input').split("\n")
 
 parsed_passwords = passwords.map { |password| password.match /(\d*)-(\d*)\s(.):\s(.*)/ }
 valid_passwords = parsed_passwords.select do |p|
-	min = p[1].to_i
-	max = p[2].to_i
+	first = p[1].to_i
+	second = p[2].to_i
 	search = p[3]
 	sample = p[4]
-	count = sample.count(search)
 	
-	count >= min && count <= max
+	(sample[first - 1] == search) ^ (sample[second - 1] == search)
 end
-
 
 puts valid_passwords.inspect
 puts valid_passwords.count
