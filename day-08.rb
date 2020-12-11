5edc6b660e5522bdb920e1993e77c324f7da9d9a
Jonathan Bennett <jonathan@jbennett.me>
2020-12-11 04:56:28 +0000

    day 08b

--- day-08.rb
+++ day-08.rb
@@ -1,6 +1,6 @@
 require 'set'
 
-instructions = File.read('day-08-input').split("\n").map do |line|
+original_instructions = File.read('day-08-input').split("\n").map do |line|
 	parts = line.split(" ")
 	
 	[
@@ -9,25 +9,48 @@ instructions = File.read('day-08-input').split("\n").map do |line|
 	]
 end
 
-accumulator = 0
-pointer = 0
-history = Set.new
+highwater = nil
+instructions = original_instructions.clone
+tries = 0
 
 loop do
-	break if history.include? pointer
-	history << pointer
-	puts "running: #{pointer}"
+	accumulator = 0
+	pointer = 0
+	history = []
 	
-	case instructions[pointer][0]
-	when "nop"
-		nil
-		pointer += 1
-	when "acc"
-		accumulator += instructions[pointer][1]
-		pointer += 1
-	when "jmp"
-		pointer += instructions[pointer][1]
+	loop do
+		break if pointer == instructions.length # termination!
+		break if history.include? pointer # infinite
+		history << pointer
+		
+		case instructions[pointer][0]
+		when "nop"
+			pointer += 1
+		when "acc"
+			accumulator += instructions[pointer][1]
+			pointer += 1
+		when "jmp"
+			pointer += instructions[pointer][1]
+		end
 	end
+	
+	if pointer == instructions.length
+		puts "program ran to completion after #{tries} tries"
+		puts "Pointer: #{pointer}"
+		puts "Acc: #{accumulator}"
+		break
+	end
+	
+	highwater = highwater.nil? ? history.length - 1: highwater - 1
+	mutation = history[highwater]
+	instructions = original_instructions.dup
+	instructions[mutation] = ["nop", original_instructions[mutation][1]] if original_instructions[mutation][0] == "jmp"
+	instructions[mutation] = ["jmp", original_instructions[mutation][1]] if original_instructions[mutation][0] == "nop"
+	
+	tries += 1
+	break if tries > 100 # infinite loops
+	break if highwater < 0
 end
 
-puts "Acc: #{accumulator}"
+puts "failed" if tries > 100
+puts "buffer underrun" if highwater < 0
