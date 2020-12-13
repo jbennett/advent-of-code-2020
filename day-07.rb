ea52245633bca1dd3f27a3188942d330a76a65af
Jonathan Bennett <jonathan@jbennett.me>
2020-12-13 18:52:56 +0000

    day 7b recursion!

--- day-07.rb
+++ day-07.rb
@@ -1,32 +1,20 @@
-rules = File.read('day-07-input').split("\n")
-rules = rules.map do |rule|
+@rules = File.read('day-07-input').split("\n")
+@rules = @rules.map do |rule|
   parts = rule.match(/(.*) bags contain (.*)./)
   name = parts[1]
   bags = parts[2].split(', ').map do |bag|
     m = bag.match(/(\d*) (.*) bag\s*/)
-    [m[2], m[1].to_i] unless m[2] == 'other bags'
+    [m[2], m[1].to_i] unless m[2] == 'other'
   end.compact.to_h
   
   [name, bags]
 end.to_h
-# puts rules.inspect
+# puts @rules.inspect
 
-bags_to_search_for = ["shiny gold"]
-holding_bags = []
-
-while !bags_to_search_for.empty? do
-  search = bags_to_search_for.pop
-  # puts "seaching for #{search}"
-  
-  rules.each do |name, bags|
-    if bags[search]
-      # puts "adding #{name}"
-      bags_to_search_for << name
-      holding_bags << name
-    else
-      # puts "did not find #{search} in #{name}"
-    end
-  end
+def bags_in_bag(bag_name)
+  @rules[bag_name].map do |name, count|
+    (bags_in_bag(name) * count) + count
+  end.sum
 end
 
-puts holding_bags.uniq.count
+puts "shiny gold contains #{bags_in_bag("shiny gold")} bags"
